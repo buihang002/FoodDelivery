d9c3f290aaca96ad5e11f64cc95d63a55a67f254
pqhuy8723 <huycan8723@gmail.com>
2025-03-06 13:24:53 +0700

    update cart ver 2

--- back-end/routers/cartRouter.js
+++ back-end/routers/cartRouter.js
@@ -4,13 +4,16 @@ import {
   addToCart,
   removeFromCart,
   getCart,
+  removeItemFromCart,
+  removeAllCart,
+  removeFromCart,
 } from "../controllers/cartController.js";
 import authMiddleware from "../middleware/auth.js";
 const cartRouter = express.Router();
 
 cartRouter.post("/add", authMiddleware, addToCart);
-cartRouter.post("/remove", authMiddleware, removeFromCart);
-
+cartRouter.post("/removeItem", authMiddleware, removeItemFromCart);
+cartRouter.post("/removeAllItem", authMiddleware, removeAllCart);
 cartRouter.get("/get", authMiddleware, getCart);
-
+cartRouter.get("/remove", authMiddleware, removeFromCart);
 export default cartRouter;
