35a0e69aac9f2f7f97691c7657a11b997e4aabc4
pqhuy8723 <huycan8723@gmail.com>
2025-03-07 11:18:44 +0700

    Xóa node_modules khỏi Git

--- front-end/src/components/FoodItem/FoodItem.js
+++ front-end/src/components/FoodItem/FoodItem.js
@@ -1,35 +1,51 @@
-import React, { useContext } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets.js'
-import { StoreContext } from '../../context/StoreContext.js'
-const FoodItem = ({id,name,price,description,image}) => {
-const{cartItems,addToCart,removeFromCart} = useContext(StoreContext);
+import React, { useContext } from "react";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets.js";
+import { StoreContext } from "../../context/StoreContext.js";
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeItemFromCart, url } =
+    useContext(StoreContext);
   return (
-    <div className='food-item'>
-        <div class="food-item-img-container">
-            <img className='food-item-image'src={image} alt=""/>
-              {!cartItems[id]
-              ?<img className='add'onClick={()=>addToCart(id)} src={assets.add_icon_white} alt=""/>
-              :<div class="food-item-counter">
-                <img onClick={() =>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
-                <p>{cartItems[id]}</p>
-                <img onClick={() =>addToCart(id)} src={assets.add_icon_green} alt=""/>
-
-              </div>
-              }
-            
+    <div className="food-item">
+      <div class="food-item-img-container">
+        <img
+          className="food-item-image"
+          src={url + "/imgaes/" + image}
+          alt=""
+        />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            onClick={() => addToCart(id)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div class="food-item-counter">
+            <img
+              onClick={() => removeItemFromCart(id)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div class="food-item-info">
+        <div class="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-        <div class="food-item-info">
-            <div class="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt=""/>
-            </div>
-            <p class="food-item-desc">{description}</p>
-            <p class="food-item-price">${price}</p>
-        </div>
-      
+        <p class="food-item-desc">{description}</p>
+        <p class="food-item-price">${price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
--- front-end/src/pages/Cart/Cart.jsx
+++ front-end/src/pages/Cart/Cart.jsx
@@ -1,11 +1,17 @@
 import { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
-
+import { assets } from "../../assets/assets.js";
 import { useNavigate } from "react-router-dom";
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
-    useContext(StoreContext);
+  const {
+    cartItems,
+    food_list,
+    removeFromCart,
+    getTotalCartAmount,
+    addToCart,
+    removeItemFromCart,
+  } = useContext(StoreContext);
   const navigate = useNavigate();
   return (
     <div className="cart">
@@ -20,26 +26,38 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
-            return (
-              <div>
-                {" "}
-                <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <p>{item.name}</p>
-                  <p>{item.price}</p>
+        {food_list
+          .filter((item) => cartItems[item._id] > 0)
+          .map((item, index) => (
+            <div key={item._id}>
+              <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <div className="quantity-controls">
+                  <img
+                    onClick={() => removeItemFromCart(item._id)}
+                    src={assets.remove_icon_red}
+                    alt=""
+                  />
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
-                  <p onClick={() => removeFromCart(item._id)} className="cross">
-                    x
-                  </p>
+                  <img
+                    onClick={() => addToCart(item._id)}
+                    src={assets.add_icon_green}
+                    alt=""
+                  />
                 </div>
-                <hr />
+                <p>{item.price * cartItems[item._id]}</p>
+                <p
+                  onClick={() => removeFromCart(item._id, true)}
+                  className="cross"
+                >
+                  x
+                </p>
               </div>
-            );
-          }
-        })}
+              <hr />
+            </div>
+          ))}
       </div>
       <div className="cart-bottom">
         <div className="cart-total">
