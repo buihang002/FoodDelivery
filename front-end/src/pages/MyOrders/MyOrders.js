6520937567a7fd5940f64a9aa0f52a3462230147
taipthe176793 <taipthe176793@fpt.edu.vn>
2025-03-09 02:08:05 +0700

    order detail modal v1.0

--- front-end/src/pages/MyOrders/MyOrders.js
+++ front-end/src/pages/MyOrders/MyOrders.js
@@ -3,10 +3,13 @@ import "./MyOrders.css";
 import { StoreContext } from "../../context/StoreContext.js";
 import axios from "axios";
 import { assets } from "../../assets/assets.js";
+import { Modal, Button } from "react-bootstrap";
 
 const MyOrders = () => {
   const { url, token } = useContext(StoreContext);
   const [data, setData] = useState([]);
+  const [modalShow, setModalShow] = React.useState(false);
+  const [selectedOrder, setSelectedOrder] = useState();
 
   const fetchOrders = async () => {
     const res = await axios.post(
@@ -15,7 +18,13 @@ const MyOrders = () => {
       { headers: { Authorization: `Bearer ${token}` } }
     );
     setData(res.data.data);
-    console.log(res.data.data);
+  };
+
+  const trackOrder = (e) => {
+    const container = e.target.closest("div .my-orders-order");
+    const orderId = container.querySelector("input[name='orderId']").value;
+    const orderDt = data.find((order) => order._id === orderId);
+    setSelectedOrder(orderDt);
   };
 
   useEffect(() => {
@@ -24,6 +33,60 @@ const MyOrders = () => {
     }
   }, [token]);
 
+  function MyOrderDetailModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Order details
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="cart-items-title cart-items-item">
+            <p>
+              <b>Image</b>
+            </p>
+            <p>
+              <b>Name</b>
+            </p>
+            <p>
+              <b>Price</b>
+            </p>
+            <p>
+              <b>Quantiy</b>
+            </p>
+            <p>
+              <b>Item total</b>
+            </p>
+          </div>
+          <hr />
+          {props.selectedOrder?.items.map((item, index) => {
+            return (
+              <div key={item._id}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image.url} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}$</p>
+                  <p>{item.quantity}</p>
+                  <p>{item.price * item.quantity}$</p>
+                </div>
+                <hr />
+              </div>
+            );
+          })}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
   return (
     <div className="my-orders">
       <h2>My Orders</h2>
@@ -31,6 +94,7 @@ const MyOrders = () => {
         {data.map((order, index) => {
           return (
             <div key={index} className="my-orders-order">
+              <input hidden name="orderId" value={order._id} />
               <img src={assets.parcel_icon} alt="" />
               <p>
                 {order.items.map((item, index) => {
@@ -46,7 +110,21 @@ const MyOrders = () => {
               <p>
                 <span>&#x25cf;</span> <b>{order.status}</b>
               </p>
-              <button>Track Order</button>
+              <button
+                onClick={(e) => {
+                  trackOrder(e);
+                  setModalShow(true);
+                }}
+              >
+                Track Order
+              </button>
+
+              <MyOrderDetailModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                selectedOrder={selectedOrder}
+                backdropClassName="transparent-backdrop"
+              />
             </div>
           );
         })}
